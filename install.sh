0757c1fb4df4e9ff645d1e08cc19a0058da61eb9
Daniel Riege <daniel.riege99@gmail.com>
2023-10-06 10:39:22 +0200

    added tpm install

--- install.sh
+++ install.sh
@@ -22,6 +22,10 @@ do
 	ln -s $PWD/$i 	~/.$i 
 done
 
+# install tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 # installing tmux plugins
 echo "Installing tmux plugins"
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
